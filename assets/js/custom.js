d53f654eeec2bdde28596920ca68b5710262f661
koukoudo <nikanasta15@gmail.com>
2023-10-03 14:17:50 +0200

    final updates for linkedin

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -61,9 +61,9 @@
             var typed = new Typed('#about-text', {
                 strings: ["My name is Nikolaos and I am a web developer based in Johannesburg, South Africa. &#127487;&#127462;",
                             "With around 10 years of programming experience, I specialize in developing and maintaining custom web sites and web applications.",
-                            "The biggest merketplace today is virtual, with over 2 billion people visiting the web each day, meaning an online presence is more necessary than ever before.",
+                            "The biggest marketplace today is virtual, with over 2 billion people visiting the web each day, meaning an online presence is more necessary than ever before.",
                             "Plus, technology can make business a whole lot easier - think automating workflows, tracking expenses, managing customers, all at the touch of a button.",
-                            "These are just some of the great ways the internet can help optimize, improve and simplify your business, and your life! .",
+                            "These are just some of the great ways the internet can help optimize, improve and simplify your business, and your life!",
                             "I am passionate about creating quality digital solutions that can handle the heavy lifting of administrative tasks and broadcast your brand's unique mission to the world.",
                             "If you have an idea in mind for a site or an app, feel free to get in touch with me using the information below - it would be my joy to help you bring it to fruition."],
                 typeSpeed: 20,
